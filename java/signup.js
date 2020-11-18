16e1753426222d19bf99296e364084f3b1ed7e3b
scroll <shrookashraf24@gmail.com>
2020-11-18 02:17:35 +0200

    added js

---
+++ java/signup.js
@@ -0,0 +1,20 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+$(document).ready(function(){
+
+var password = document.getElementById("password") ,
+ confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+});
