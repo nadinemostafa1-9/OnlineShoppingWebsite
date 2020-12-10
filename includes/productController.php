fb48b2df259a5eb94318bf7f386a376e117429b6
NouranHussein <70723581+NouranHussein@users.noreply.github.com>
2020-12-10 23:52:28 +0200

    Update productController.php

--- includes/productController.php
+++ includes/productController.php
@@ -1,6 +1,6 @@
 
 <?php
-
+require 'product.php';
   function connecting(){
   require_once('db.php');
   $mdb=new db();
@@ -20,32 +20,14 @@ return $product;
 }
 }
  function displayProduct($product){
-  echo '<tr><td><img height="100" width="100" src="data:image/jpg;base64,'.base64_encode($product->img).'"/></td</tr>';
-  echo '<br></br>';
-    echo $product->name . ' ';
-    echo $product->price . '$';
-    echo '<br></br>';
-}
-
-//Display all products in the main page
-function displayAllProducts(){
-  $mPDO=connecting();
-  $get_product = "SELECT * FROM products ORDER BY RAND() LIMIT 30;";
-  $run = $mPDO->prepare($get_product);
-  $run->execute();
-  while ($row = $run->fetch()) {
-
-    $product = new Product($row['id'],$row['name'],$row['category'],
-   $row['price'],$row['count'],$row['image'],$row['keywords'],$row['description']);
-//Remember to add ID as GET....
    echo '<div class = "col-md-3 col-sm-6">
           <div class="card">
           <a href="cardtest.php">
-          <img src="data:image/jpg;base64,'.base64_encode($product->img).'" class="card-img-top" alt="product"/>
+          <img src="data:image/jpg;base64,'.base64_encode($product->getImage()).'" class="card-img-top" alt="product"/>
           </a>
           <div class="card-body">
-          <p class="card-text">' .$product->name.'</p>
-          <p class="item-price"> $'.$product->price.'</p>
+          <p class="card-text">' .$product->getName().'</p>
+          <p class="item-price"> $'.$product->getPrice().'</p>
           </div>
           <div class="add-btn">
             <button href="#" class="card-btn">Add to Cart</button>
@@ -55,13 +37,10 @@ function displayAllProducts(){
         </div>
 
    ';
-  }
-return true;
 }
-
-function displayProductsByCategory($bywhat){
+function displayProductsByCategory($theValue){
   $mPDO=connecting();
-  $q='SELECT * FROM `products`';
+  $q='SELECT * FROM `products` WHERE ' . 'category' . ' LIKE  ' . "'$theValue'";
   $prepare=$mPDO->prepare($q);
   $prepare->execute();
   while($r=$prepare->fetch(PDO::FETCH_ASSOC)){
@@ -71,3 +50,17 @@ function displayProductsByCategory($bywhat){
    echo '<br></br>';
     }
   }
+  function displayAllProducts(){
+  $mPDO=connecting();
+  $get_product = "SELECT * FROM products ORDER BY RAND() LIMIT 30";
+  $run = $mPDO->prepare($get_product);
+  $run->execute();
+  while ($row = $run->fetch(PDO::FETCH_ASSOC)) {
+
+    $product = new Product($row['id'],$row['name'],$row['category'],
+   $row['price'],$row['count'],$row['image'],$row['keywords'],$row['description']);
+//Remember to add ID as GET....
+   displayProduct($product);
+   }
+return true;
+}
