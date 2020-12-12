59f67f8771c82fe374e2e75f820cd25cdd96cb6b
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2020-12-13 00:00:14 +0200

    Update cartController.php

--- includes/cartController.php
+++ includes/cartController.php
@@ -1,17 +1,17 @@
 <?php
-include 'includes/class-autoload.inc.php';
+include 'class-autoload.inc.php';
 
 Session::init();
 if(isset($_POST["add_to_cart"]))
 {
 	$mPDO=new db();
-	$stmt = $mPDO->connect()->prepare("INSERT INTO cart (id,customer_id,quantity) VALUES
-	(:id,:customer_id,:quantity)");
+	$stmt = $mPDO->connect()->prepare("INSERT INTO cart (product_id,customer_id,quantity) VALUES
+	(:product_id,:customer_id,:quantity)");
 	$stmt->execute(array(
-		":id" => $_POST['item_number'],
+		":product_id" => $_GET['id'],
     ":customer_id" => Session::get('customer_id'),
 		":quantity" =>$_POST['qty']
   ));
-	header("Location: HOME.php");
+	header("Location: ../HOME.php");
 	return;
 }
