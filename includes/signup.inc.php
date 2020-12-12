2557307c175dcabc5d32a7d8ab6b9ebe4fb4e6f8
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2020-12-12 21:33:07 +0200

    edit some lines

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -3,16 +3,20 @@ include 'class-autoload.inc.php';
 Session::init();
 
 if(isset($_POST['signup'])){
-
-
-
-    $user = new Customer($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['password']);
+if($_POST['type'] == 'customer'){
+    $user = new Customer($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['password'], $_POST['type']);
     if($user->CheckEmail())
     {
     $user->setUser();
       Session::set('success',"You have signed up");
+      header("Location: ../HOME.php");
+      return;
+  }else {
+    Session::set('error',"This email is already used");
+    header("Location: ../signup.php");
+    return;
   }
-
-
+//add seller condition
+}
 }
 ?>
