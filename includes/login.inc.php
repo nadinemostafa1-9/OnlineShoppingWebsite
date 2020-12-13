180912a0afcbf550620cea30586b7beb76ce5c31
salma77 <47184333+salma77@users.noreply.github.com>
2020-12-13 21:50:42 +0200

    added seller part in login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -8,7 +8,11 @@ if(isset($_POST['login'])){
     if(Customer::CheckLogin($_POST['em'],$_POST['pass'])){
     header("Location: ../HOME.php");
     return;
-  }//else if seller
+  }
+  else if(Seller::CheckLogin($_POST['em'], $_POST['pass'])){
+      header("Location: ../HOME.php"); 
+      return;
+  }
   else {
     Session::set('error', "Incorrect Email or password");
     header("Location: ../login.php");
