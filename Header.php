113c48db74a9fb08a3f5198e8d532122a3dbbecc
Salma-Hamed <71777717+Salma-Hamed@users.noreply.github.com>
2020-12-18 20:10:38 +0200

    Update Header.php

--- Header.php
+++ Header.php
@@ -13,7 +13,7 @@ Session::init();
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <link rel="stylesheet" type="text/css" href="css/Header.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
 	<title>Header</title>
 </head>
 <body>
