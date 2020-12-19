f3d699a2989830b7224a91c21d18145e472ec450
nourhan91 <74134249+nourhan91@users.noreply.github.com>
2020-12-19 18:59:30 +0200

    Update cart.php

--- classes/cart.php
+++ classes/cart.php
@@ -2,10 +2,10 @@
 require "cartItem.php";
 class Cart{
 
-private  $items=[];
+private  $items=array();
 
 public function getItems(){
-  return $items;
+  return $this->items;
 }
 public function setItems($items){
   $this->items=$items;
@@ -13,7 +13,7 @@ public function setItems($items){
 public function addProduct($product,$amount){
 $f=0;
   foreach ($this->items as $item) {
-    if($item->getProduct()->id==$product->id)
+    if($item->getProduct()->getID()==$product->getID())
     {
       $cartItem=$item;
       $f=1;
@@ -21,7 +21,7 @@ $f=0;
   }
   if(!$f){
   $cartItem=new CartItem($product,0);
-$this->items[$product->id]=$cartItem;
+$this->items[$product->getID()]=$cartItem;
 }
 $cartItem->increaseQuantity($amount);
 return $cartItem;
@@ -36,12 +36,12 @@ public function getTotalQuantity(){
 public function getTotalSum(){
   $sum=0;
   foreach($this->items as $item){
-    $sum+=$item->getQuantity()*$item->getProduct()->price;
+    $sum+=$item->getQuantity()*$item->getProduct()->getPrice();
   }
   return $sum;
 }
 public function removeProduct($product){
-  unset($this->items[$product->id]);
+  unset($this->items[$product->getID()]);
 
 }
 }
