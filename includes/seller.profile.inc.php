a541ce7a03605d7b894f28b5b02e7daeabd89107
salma77 <47184333+salma77@users.noreply.github.com>
2020-12-25 14:35:12 +0200

    added seller.profile.inc.php

---
+++ includes/seller.profile.inc.php
@@ -0,0 +1,41 @@
+<?php
+include 'class-autoload.inc.php';
+
+Session::init();
+
+if(isset($_POST['cancel'])){
+  header("Location: ../index.php");
+  return;
+}
+else if(isset($_POST['save'])){
+    if($_POST['password']==null && $_POST['new']==null) {
+
+        if(Seller::updateInfo($_POST['first_name'],$_POST['last_name'],$_POST['email'])){
+            Session::set('success',"Changes made");
+            header("Location: ../profile.php");
+            return;
+        }
+        else if (!Seller::updateInfo($_POST['first_name'],$_POST['last_name'],$_POST['email'])) {
+            Session::set('error',"This Email is already used");
+            header("Location: ../profile.php");
+            return;
+        }
+    }
+    else if($_POST['password']!=null && $_POST['new']!=null){
+        if(Seller::updateAll($_POST['first_name'],$_POST['last_name'],$_POST['email'],$_POST['password'],$_POST['new'])){
+            Session::set('success', "Password Changed");
+            header("Location: ../profile.php");
+            return;
+        }
+        else if(!Seller::updateAll($_POST['first_name'],$_POST['last_name'],$_POST['email'],$_POST['password'],$_POST['new'])) {
+            Session::set('error', "Wrong password");
+            header("Location: ../profile.php");
+        }
+    }
+    else{
+        Session::set('error',"Please fill in all the fields");
+        header("Location: ../profile.php");
+        }
+ }
+
+?>
