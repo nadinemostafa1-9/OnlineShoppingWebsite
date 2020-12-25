75580a231e68c42bc356826117b0bbb87bebe254
salma77 <47184333+salma77@users.noreply.github.com>
2020-12-25 14:53:19 +0200

    edited insert_product.php

--- insert_product.php
+++ insert_product.php
@@ -3,8 +3,7 @@ require "db.php";
 require "product.php";
 if(isset($_POST['submit'])){ //name of the insert product button
     $product_title=$_POST['product_title'];
-    $product_cat=$_POST['product_cat'];
-    $cat=$_POST['cat'];
+    $product_cat=$_POST['category'];
     $product_price=$_POST['product_price'];
     $product_count=$_POST['product_count'];
     $product_keywords=$_POST['product_keywords'];
@@ -15,15 +14,14 @@ if(isset($_POST['submit'])){ //name of the insert product button
 
     move_uploaded_file($temp_name1, "###/$product_img1"); //instead of ### put the name of the file which contains the images
     $dbObj = new db();
-    $insert_product_query="INSERT INTO products (p_cat_id, cat_id, date, product_title, product_img1,
-    product_img2, product_img3, product_price, product_desc, product_keywords) VALUES('$product_cat',
-    '$cat', NOW(), '$product_title', '$product_img1', '$product_img2', '$product_img3', '$product_price',
-    '$product_desc', '$product_keywords')";
+    $insert_product_query="INSERT INTO products (p_cat, product_title, product_img1,
+    product_price, product_desc, product_keywords, product_count) VALUES('$product_cat'
+        , '$product_title', '$product_img1', '$product_price','$product_desc',
+     '$product_keywords', '$product_count')";
     $stmt = $dbObj->connect()->prepare($insert_product_query);
     $data = $stmt->fetch();
     //Construct product object!!
     product($id,$product_title,$product_cat,$product_price,$product_count, $product_img1,$product_keywords,$product_desc);
-    //the next code assumes con is the connection function
 
     if($data == false)
         return false;
