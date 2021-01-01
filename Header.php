bcb069ed1ebc2c06e79ffa95f3615697bf710c68
Salma-Hamed <71777717+Salma-Hamed@users.noreply.github.com>
2021-01-01 21:29:48 +0200

    Update Header.php

--- Header.php
+++ Header.php
@@ -1,25 +1,9 @@
 <?php
-include ("includes/class-autoload.inc.php");
+include ("classes/session.php");
 Session::init();
  ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<!--BOOTSTRAP meta-->
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!--BOOTSTRAP stylesheet-->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <link rel="stylesheet" type="text/css" href="css/main.css">
-	<title>Header</title>
-</head>
-<body>
-  <!--BOOTSTRAP -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   <!------------------------------------Guest navbar------------------------------------------------>
 <?php if (!Session::logged()) {?>
   <nav class="navbar navbar-dark bg-dark">
@@ -72,7 +56,7 @@ Session::init();
 
       <div class="nav_bar">
        <!----------------------------Home Page---------------------->
-			 <a href="HOME.html" target="_self">
+			 <a href="HOME.php" target="_self">
 			 <svg width="1em" height="20px" viewBox="0 2 16 16" class="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   		 <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/><path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
 			 </svg>Home</a>
@@ -98,8 +82,8 @@ Session::init();
         <button type="submit"><i class="fa fa-search"></i></button>
       </form>
     	<div class="log_in">
-        <a href="shrook2.html">Sign Up</a>
-        <a href="#">Login</a>
+        <a href="signup.php">Sign Up</a>
+        <a href="login.php">Login</a>
     	</div>
 	  </div>
 	</nav>
@@ -158,7 +142,7 @@ Session::init();
 
       <div class="nav_bar">
        <!----------------------------Home Page---------------------->
-			 <a href="HOME.html" target="_self">
+			 <a href="HOME.php" target="_self">
 			 <svg width="1em" height="20px" viewBox="0 2 16 16" class="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   		 <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/><path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
 			 </svg>Home</a>
@@ -177,7 +161,7 @@ Session::init();
   		 <path fill-rule="evenodd" d="M3 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 7.586 1H3zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/><path d="M1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
 			 </svg>Offers</a>
 
-       <a href="">
+       <a href="Cart.php">
 			 <svg width="1em" height="20px" viewBox="0 2 16 16" class="bi bi-cart-check-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
        <path fill-rule="evenodd" d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM4 14a1 1 0 1 1 2 0 1 1 0 0 1-2 0zm7 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0zm.354-7.646a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
        </svg>My Cart</a>
@@ -188,11 +172,9 @@ Session::init();
         <button type="submit"><i class="fa fa-search"></i></button>
       </form>
     	<div class="log_out">
-        <a href="#">Username</a>
-        <a href="HOME.php">Log out</a>
+        <a href="profile.php"><?php echo (Session::get('fname') .Session::get('lname'))?></a>
+        <a href="logout.php">  Log out</a>
     	</div>
 	  </div>
 	</nav>
 <?php } ?>
-</body>
-</html>
