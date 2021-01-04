2d4a17b9891e39fe8df222549ec6ca4f80134fd9
NouranHussein <70723581+NouranHussein@users.noreply.github.com>
2021-01-04 12:10:51 +0200

    Update cartFun.php

--- includes/cartFun.php
+++ includes/cartFun.php
@@ -1,31 +1,5 @@
 <?php
-include('C:\MAMP\htdocs\e commerce proj/includes/productController.php');
-
-function updatingCart($id,$cust_id,$cart,$amount){
-	$cart_items=$cart->getItems();
-	$f=1;
-	foreach (	$cart_items as $value) {
-		if($value->getProduct()->getID()==$id){
-			$f=0;
-		}
-	}
-	if($f){
-		$mPDO=new db();
-			$stmt = $mPDO->connect()->prepare("INSERT INTO cart(product_id,customer_id,quantity) VALUES
-			(:product_id,:customer_id,:quantity)");
-		  $stmt->bindParam(':product_id',$id);
-		  $stmt->bindParam(':customer_id',$cust_id);
-		  $stmt->bindParam(':quantity',$amount);
-			$stmt->execute();
-			$product=getProductBy('id',$id);
-			$cart->addProduct($product,1);
-			return true;
-	}else {
-			return false;
-	}
-
-}
-//items in cart
+require "productController.php";
  function gettingCart($customer_id){
 	$mPDO=new db();
   $cart=new Cart();
@@ -42,18 +16,52 @@ function updatingCart($id,$cust_id,$cart,$amount){
 	}
 	return $cart;
 }
+ function updatingCart($cart,$id,$cust_id){
+	$cart_items=$cart->getItems();
+	$f=1;
+	foreach (	$cart_items as $value) {
+		if($value->getProduct()->getID()==$id){
+			$f=0;
 
-//removeItem
-function removeItem($id,$cart, $cust_id){
+		}
+	}
+	if($f){
+	if(isset($_POST["add_to_cart"])){
+	$mPDO=new db();
+	$stmt = $mPDO->connect()->prepare("INSERT INTO cart(product_id,customer_id,quantity) VALUES
+	(:product_id,:customer_id,:quantity)");
+  $stmt->bindParam(':product_id',$id);
+  $stmt->bindParam(':customer_id',$cust_id);
+  $stmt->bindParam(':quantity',$_POST['qty']);
+	$stmt->execute();
+	$product=getProductBy('id',$id);
+	$cart->addProduct($product,1);
+}}
+return $cart;
+}
+ function displayCart($cart){
+$items=	$cart->getItems();
+foreach ($items as $value) {
+	displayProduct($value->getProduct());
+
+}
+}
+
+function removeCart($cust_id,$cart){
+  $mPDO=new db();
+  $q='DELETE  FROM `cart` WHERE customer_id = ' . $cust_id;
+  $prepare=$mPDO->connect()->prepare($q);
+  $prepare->execute();
+   $cart->removeAllProducts();
+ return $cart;
+}
+ function removeItem($id,$cart, $cust_id){
    $product=getProductBy('id',$id);
 	 $mPDO=new db();
-	 $q='DELETE FROM cart WHERE product_id = ' . $id .' AND customer_id = ' . $cust_id;
+	 $q='DELETE FROM `cart` WHERE product_id = ' . $id .' AND customer_id = ' . $cust_id;
 	 $prepare=$mPDO->connect()->prepare($q);
 	 $prepare->execute();
-  if($product){
+  if($product)
     $cart->removeProduct($product);
-	return true;}
-	else {
-		return false;
-	}
+	return $cart;
 }
