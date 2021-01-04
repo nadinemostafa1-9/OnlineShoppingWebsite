818f10ef48babb4b0caf7d4c94e69f9eedc28c5c
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-01-04 15:34:03 +0200

    Update cartController.php

--- includes/cartController.php
+++ includes/cartController.php
@@ -7,14 +7,10 @@ Session::init();
 if(isset($_POST["remove"]) && isset($_POST['item_number'])){
 		 $cart=gettingCart(Session::get('customer_id'));
 
-		if(removeItem($_POST['item_number'],$cart,Session::get('customer_id'))){
+		removeItem($_POST['item_number'],$cart,Session::get('customer_id'));
 			echo '<script>alert("The item has been removed")
 			window.location.replace("../Cart.php");
 			</script>';
-		}else {
-			echo '<script>alert("Something error has happened")
-			window.location.replace("../Cart.php");
-			</script>';		}
 }
 
 
@@ -22,7 +18,9 @@ if(isset($_POST["remove"]) && isset($_POST['item_number'])){
 if(isset($_POST["add_to_cart"]))
 {
 	$cart = $cart=gettingCart(Session::get('customer_id'));
-	if(updatingCart($_GET['id'],Session::get('customer_id'),$cart,$_POST['qty'])){
+	$f = 1;
+	updatingCart($_GET['id'],Session::get('customer_id'),$cart,$_POST['qty'],$f);
+	if($f){
 	echo '<script>alert("The item has been successfully added")
 				window.location.replace("../HOME.php");
 	</script>';
