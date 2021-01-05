e5ddd5b8e288f12dd277c0064cf0673abe789e38
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-01-05 21:30:20 +0200

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,10 +7,11 @@ if($_POST['type'] == 'customer'){
     $user = new Customer($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['password'], $_POST['type']);
     if($user->CheckEmail())
     {
-    $user->setUser();
-      Session::set('success',"You have signed up");
+      if($user->setUser()){
+    if(Customer::CheckLogin($_POST['email'],$_POST['password'])){
       header("Location: ../HOME.php");
       return;
+    	}}
   }else {
     Session::set('error',"This email is already used");
     header("Location: ../signup.php");
