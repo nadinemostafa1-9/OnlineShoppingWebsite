8c617110d980a0bd6bdfc725953ed26675b21ae2
salma77 <salmaabdelfatah77@gmail.com>
2021-01-08 18:53:12 +0200

    edited seller_header

--- Seller.php
+++ Seller.php
@@ -1,3 +1,7 @@
+<?php
+ include ("includes/productController.php");
+ Session::init();
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +11,7 @@
 	<!--BOOTSTRAP stylesheet-->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	
+
    <link rel="stylesheet" href="css/Seller.css">
 
 	<title>Seller Page</title>
@@ -16,14 +20,14 @@
   <!--BOOTSTRAP -->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    
+
 
 
     <!--------------------------------------------------------------------navigation bar----------------------------------------------------------------->
   <nav class="navbar navbar-dark bg-dark">
-	  	
+
     <div class="pagetop">
-		  
+
       <div id="mySidebar" class="sidebar">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <button class="dropdown-btn">Clothes
@@ -44,7 +48,7 @@
       <div id="main">
         <button class="openbtn" onclick="openNav()">&#9776;</button>
       </div>
- 
+
       <script>
         var dropdown = document.getElementsByClassName("dropdown-btn");
         var i;
@@ -67,10 +71,10 @@
         document.getElementById("main").style.marginLeft = "0";
       }
       </script>
-      
+
       <div class="nav_bar">
        <!----------------------------Home Page---------------------->
-			 <a href="HOME.html" target="_self"> 
+			 <a href="HOME.html" target="_self">
 			 <svg width="1em" height="20px" viewBox="0 2 16 16" class="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   		 <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/><path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
 			 </svg>Home</a>
@@ -83,13 +87,13 @@
 			 <svg width="1em" height="20px" viewBox="0 2 16 16" class="bi bi-question-octagon-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
        <path fill-rule="evenodd" d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM5.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z"/>
        </svg>About Us</a>
-			 
-       
+
+
        <a href="">
 			 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="20px" fill="currentColor" class="bi bi-file-earmark-plus-fill" viewBox="0 2 16 16">
        <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm7.5 1.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V11a.5.5 0 0 0 1 0V9.5H10a.5.5 0 0 0 0-1H8.5V7z"/>
        </svg>Add Product</a>
-		  
+
       </div>
 
     	<div class="log_out">
@@ -100,7 +104,7 @@
 	</nav>
 
 
-	
+
   <!--------------------------------------------------------------------home page image slider using bootstrap------------------------------------------------>
   <div class="image_slider">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -131,10 +135,10 @@
     </div>
   </div>
   <!-------------------------------------------------------------------------------------------------------------------------------------------------------------->
-   
-  
+
+
   <div class="card-group">
-  
+
       <!-------image1------>
       <div class="col-md-3">
         <div class="card">
@@ -147,7 +151,7 @@
           </div>
         </div>
       </div>
-    
+
 
       <!-------image2------>
       <div class="col-md-3">
@@ -186,7 +190,7 @@
             <p class="item-price">$Price</p>
           </div>
         </div>
-      </div>      
+      </div>
   </div>
 
   <div class="card-group">
@@ -203,21 +207,8 @@
         </div>
       </div>
     </div>
-    <!-------image6------>  
-    <div class="col-md-3">  
-      <div class="card">
-        <a href="cardtest.html">
-          <img src="8.PNG" class="card-img-top" alt="...">
-        </a>
-        <div class="card-body">
-          <h3 class="card-title"> Card title</h3>
-          <p class="item-price">$Price</p>
-        </div>
-      </div>
-    </div>
-     
-    <!-------image7------>
-    <div class="col-md-3">  
+    <!-------image6------>
+    <div class="col-md-3">
       <div class="card">
         <a href="cardtest.html">
           <img src="8.PNG" class="card-img-top" alt="...">
@@ -229,7 +220,7 @@
       </div>
     </div>
 
-    <!-------image8------> 
+    <!-------image7------>
     <div class="col-md-3">
       <div class="card">
         <a href="cardtest.html">
@@ -240,9 +231,22 @@
           <p class="item-price">$Price</p>
         </div>
       </div>
-    </div> 
+    </div>
+
+    <!-------image8------>
+    <div class="col-md-3">
+      <div class="card">
+        <a href="cardtest.html">
+          <img src="8.PNG" class="card-img-top" alt="...">
+        </a>
+        <div class="card-body">
+          <h3 class="card-title"> Card title</h3>
+          <p class="item-price">$Price</p>
+        </div>
+      </div>
+    </div>
   </div>
-  
+
   <div class="footer">
     <br>
     <p>Shoppera is an online store where you can get anything you imagine. While using Shoppera, you agree to have read and accepted our terms of use, cookie and privacy policy.</p>
