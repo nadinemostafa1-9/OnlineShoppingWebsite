f2a276b2bcd00c28cac4c7363ac23da3b0e290ef
salma77 <47184333+salma77@users.noreply.github.com>
2021-01-08 02:10:22 +0200

    corrected inserting image for product

--- insert_product.php
+++ insert_product.php
@@ -1,27 +1,35 @@
 <?php
-require "db.php";
-require "product.php";
-if(isset($_POST['submit'])){ //name of the insert product button
+require_once "db.php";
+require_once "product.php";
+if(isset($_POST['submit'])){ 
     $product_title=$_POST['product_title'];
     $product_cat=$_POST['category'];
     $product_price=$_POST['product_price'];
     $product_count=$_POST['product_count'];
     $product_keywords=$_POST['product_keywords'];
     $product_desc=$_POST['prod_desc'];
-    $product_img1=$_FILES['product_img']['name'];
+    $product_img=$_FILES['product_img']['name'];
 
-    $temp_name1=$_FILES['product_img1']['tmp_name'];
+    $temp_name=$_FILES['product_img']['tmp_name'];
 
-    move_uploaded_file($temp_name1, "Downloads/$product_img1"); //instead of ### put the name of the file which contains the images
     $dbObj = new db();
-    $insert_product_query="INSERT INTO products (p_cat, product_title, product_img1,
+    $insert_product_query="INSERT INTO products (p_cat, product_title,
     product_price, product_desc, product_keywords, product_count) VALUES('$product_cat'
-        , '$product_title', '$product_img1', '$product_price','$product_desc',
-     '$product_keywords', '$product_count')";
+    , '$product_title', '$product_price','$product_desc','$product_keywords', '$product_count')";
     $stmt = $dbObj->connect()->prepare($insert_product_query);
     $data = $stmt->fetch();
+    //to insert the image
+    if (is_uploaded_file($temp_name) {
+        $imgData=addslashes(file_get_contents($temp_name))
+        $imageProp=getimageSize($temp_name);
+        $img_sql="INSERT INTO products (image) VALUES('{$imgData}')";
+        $stmt=$dbObj->connect()->prepare($img_sql);
+        $data=$stmt->fetch();
+        if($data == false)
+            return false;
+    }
     //Construct product object!!
-    product($id,$product_title,$product_cat,$product_price,$product_count, $product_img1,$product_keywords,$product_desc);
+    product($id,$product_title,$product_cat,$product_price,$product_count, $product_img,$product_keywords,$product_desc);
 
     if($data == false)
         return false;
