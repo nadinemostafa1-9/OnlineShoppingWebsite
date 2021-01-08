bddaa81a8e18f994efa121ca3830eab4ec389d56
salma77 <>
2021-01-08 17:50:02 +0200

    modified insert_product.php

--- insert_product.php
+++ insert_product.php
@@ -14,18 +14,26 @@ if(isset($_POST['submit'])){
     $temp_name=$_FILES['product_img']['tmp_name'];
 
     $dbObj = new db();
+    if (count($_FILES) > 0) {
+        if (is_uploaded_file($temp_name) {
+            $imgData = addslashes(file_get_contents($temp_name);
+            $imageProperties = getimageSize($temp_name);
+
+            $img_sql = "INSERT INTO output_images(imageType ,imageData)
+    	       VALUES('{$imageProperties['mime']}', '{$imgData}')";
+               $stmt = $dbObj->connect()->prepare($insert_product_query);
+               $data = $stmt->fetch();
+        }
+    }
     $insert_product_query="INSERT INTO products (sellerID, p_cat, product_title,
     product_price, product_desc, product_keywords, product_count) VALUES('$seller_id', '$product_cat'
     , '$product_title', '$product_price','$product_desc','$product_keywords', '$product_count')";
     $stmt = $dbObj->connect()->prepare($insert_product_query);
     $data = $stmt->fetch();
     //to insert the image
-    $img_sql = "INSERT INTO products (image) VALUES (LOAD_FILE('E:/Prog/XAMPP/htdocs/img//$temp_name'))" //place where image is saved on the web server
-    move_uploaded_file($temp_name, "E:/Prog/XAMPP/htdocs/imgdump//$product_img");
-
-    if($data == false)
-        return false;
-    else{
+    //$img_sql = "INSERT INTO products (image) VALUES (LOAD_FILE('E:/Prog/XAMPP/htdocs/img//$temp_name'))" //place where image is saved on the web server
+    
+    if($data == true)
         echo "<script>alert('Product Inserted successfully')</script>";
         echo "<script>window.open('insert_product.php')</script>";
     }
