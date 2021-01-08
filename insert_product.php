6c09750f81e2651d9b20517ba901326dc63b5752
salma77 <salmaabdelfatah77@gmail.com>
2021-01-08 20:16:26 +0200

    edited insert product

--- insert_product.php
+++ insert_product.php
@@ -18,12 +18,21 @@ if(isset($_POST['submit'])){
         if (is_uploaded_file($temp_name)){
             $imgData = addslashes(file_get_contents($temp_name));
             $imageProperties = getimageSize($temp_name);
-
             $img_sql = "INSERT INTO products (image) VALUES('{$imgData}')";
-               $stmt = $dbObj->connect()->prepare($insert_product_query);
-               $data = $stmt->fetch();
+            $stmt = $dbObj->connect()->prepare($insert_product_query);
+            $data = $stmt->fetch();
         }
     }
+    //check whether image is uploaded
+    $targetDir = "E:\phppot_uploads";
+    if(is_array($_FILES)) {
+        if(is_uploaded_file($temp_name)) {
+            if(move_uploaded_file($temp_name,"$targetDir/".$_FILES['product_img']['tmp_name'])) {
+                echo "File uploaded successfully";
+            }
+        }
+    }
+    //------end upload
     $insert_product_query="INSERT INTO products (sellerID, p_cat, product_title,
     product_price, product_desc, product_keywords, product_count) VALUES('$seller_id', '$product_cat'
     , '$product_title', '$product_price','$product_desc','$product_keywords', '$product_count')";
