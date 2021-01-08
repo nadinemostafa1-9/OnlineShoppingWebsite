4f211ee1f60971a1584e70b92ccc317e5e141785
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-01-08 20:20:09 +0200

    Update report a problem.php

--- report a problem.php
+++ report a problem.php
@@ -12,7 +12,7 @@ Session::logged();
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"><!--for icons--->
 <link rel="stylesheet" type="text/css"href="css/problem.css">
-<title>seller profile</title>
+<title>report a problem</title>
 </head>
   <body>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
