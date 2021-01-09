125ca99d36a154a81f8044cafde7fd230c4db18c
salma77 <salmaabdelfatah77@gmail.com>
2021-01-09 16:15:20 +0200

    testing

--- includes/insert_product.php
+++ includes/insert_product.php
@@ -1,39 +1,44 @@
 <?php
 require_once ('db.php');
 require_once ('product.php');
-if(isset($_POST['submit'])){
-    $seller_id = $_SESSION["seller_id"];
-    $product_title=$_POST['product_title'];
-    $product_cat=$_POST['category'];
-    $product_price=$_POST['product_price'];
-    $product_count=$_POST['product_count'];
-    $product_keywords=$_POST['product_keywords'];
-    $product_desc=$_POST['prod_desc'];
-    //$product_img=$_FILES['product_img']['name'];
+function productInsert($data, $file)
+        {
+            $seller_id = $_SESSION["seller_id"];
+            $product_title=$data['product_title'];
+            $product_cat=$data['category'];
+            $product_price=$data['product_price'];
+            $product_count=$data['product_count'];
+            $product_keywords=$data['product_keywords'];
+            $product_desc=$data['prod_desc'];
 
-    $temp_name=$_FILES['product_img']['tmp_name'];
-/*
-    $dbObj = new db();
-    if (count($_FILES) > 0) {
-        if (is_uploaded_file($temp_name)){
-            $imgData = addslashes(file_get_contents($temp_name));
-            $imageProperties = getimageSize($temp_name);
+            $permited  = array('jpg', 'jpeg', 'png', 'gif');
+            $file_name = $file['product_img']['name'];
+            $file_temp = $file['product_img']['tmp_name'];
 
-            $img_sql = "INSERT INTO products (image)
-    	       VALUES('{$imgData}')";
-               $stmt = $dbObj->connect()->prepare($insert_product_query);
-               $data = $stmt->fetch();
+            $div = explode('.', $file_name);
+            $file_ext = strtolower(end($div));
+            $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+            $uploaded_image = "upload/".$unique_image;
+
+            if (empty($file_name)) {
+                echo "<span class='error'>Please Select any Image !</span>";
+            } elseif (in_array($file_ext, $permited) === false) {
+                echo "<span class='error'>You can upload only:-".implode(', ', $permited)."</span>";
+            } else {
+                move_uploaded_file($file_temp, $uploaded_image);
+                $insert_product_query="INSERT INTO products (sellerID, p_cat, product_title,
+                product_price, product_desc, product_keywords, product_count, img) VALUES('$seller_id', '$product_cat'
+                , '$product_title', '$product_price','$product_desc','$product_keywords', '$product_count', $uploaded_image)";
+                $stmt = $dbObj->connect()->prepare($insert_product_query);
+                $data = $stmt->fetch();
+                if($data == true){
+                    echo "<script>alert('Product Inserted successfully')</script>";
+                    echo "<script>window.open('insert_product.php')</script>";
+                }
+            }
+
+            Seller:: updateRank();
         }
-    }*/
-    $insert_product_query="INSERT INTO products (sellerID, p_cat, product_title,
-    product_price, product_desc, product_keywords, product_count, img) VALUES('$seller_id', '$product_cat'
-    , '$product_title', '$product_price','$product_desc','$product_keywords', '$product_count', 'LOAD_FILE('E:/Images/$temp_name')')";
-    $stmt = $dbObj->connect()->prepare($insert_product_query);
-    $data = $stmt->fetch();
-
-    if($data == true){
-        echo "<script>alert('Product Inserted successfully')</script>";
-        echo "<script>window.open('insert_product.php')</script>";
-    }
-    Seller:: updateRank();
-}
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+            productInsert($_POST, $_FILES);
+        }
