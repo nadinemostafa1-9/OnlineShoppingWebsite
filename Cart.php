d1650fa83d93e3d757e202d8d69de6caeba6e81e
Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
2021-01-10 01:29:33 +0200

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -23,7 +23,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
     <!--------------------------------------------------------------------navigation bar----------------------------------------------------------------->
-    <?php include ("header.php");
+    <?php include ("Header.php");
     ?>
 
 
@@ -66,5 +66,6 @@ displayCart($cart);
                   <?php } ?>
 
   </div>
+		<?php include ("Footer.php")?>
         </body>
     </html>
