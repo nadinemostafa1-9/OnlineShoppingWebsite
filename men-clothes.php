385454e9eb808851434698123b2221327d559622
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-01-10 18:16:22 +0200

    Update men-clothes.php

--- men-clothes.php
+++ men-clothes.php
@@ -29,11 +29,6 @@
 
 <?php displayProductsByCategory('men-clothes') ?>
 </div>
-  <div class="footer">
-    <br>
-    <p>Shoppera is an online store where you can get anything you imagine. While using Shoppera, you agree to have read and accepted our terms of use, cookie and privacy policy.</p>
-    <br>
-    <p>All rights reserved.</p>
-  </div>
+ <? php include ("Footer.php") ?>
 </body>
 </html>
