34194d5115b7ae0e599c105c2043a4bf24fd15cb
Salma-Hamed <71777717+Salma-Hamed@users.noreply.github.com>
2021-01-10 03:11:03 +0200

    Update Add_product.php

--- Add_product.php
+++ Add_product.php
@@ -6,9 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!--BOOTSTRAP stylesheet-->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-  <link rel="stylesheet" type="text/css" href="css/Add Product.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!--Page style sheet-->
+  	<link rel="stylesheet" type="text/css" href="css/Add Product.css">
+	<!--Navbar style sheet-->
+  	<link rel="stylesheet" type="text/css" href="css/Seller Header.css">
 
 	<title>Add Product</title>
 </head>
@@ -16,7 +18,7 @@
   <!--BOOTSTRAP -->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
+  <script src="js/Shipping Information.js"></script>
  <nav class="navbar navbar-dark bg-dark">
 	  	
     <div class="pagetop">
@@ -147,19 +149,6 @@
     <button type="reset" class="cancel-btn">Cancel</button>
   </div>
 </form>
-
-<!-----------------------------------Select category-------------------------------->
-<script>
-  window.onload = function() {
-  var categorysel = document.getElementById("category");
-  for (var x in subjectObject) {
-    categorySel.options[categorySel.options.length] = new Option(x, x);
-  }
-  categorysel.onchange = function() {
-    categorySel.length = 1;
-  }
-}
-</script>
 
   <div class="footer">
     <br>
