c6ed6520595fce7b736d564a62719ee2b2e6f53c
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-01-10 02:15:20 +0200

    Update report a problem.php

--- report a problem.php
+++ report a problem.php
@@ -82,8 +82,6 @@ Session::logged();
 			 <svg width="1em" height="20px" viewBox="0 2 16 16" class="bi bi-question-octagon-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
        <path fill-rule="evenodd" d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM5.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z"/>
        </svg>About Us</a>
-			 
-
        <a href="">
    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
@@ -102,7 +100,6 @@ Session::logged();
     	</div>
 	  </div>
 	</nav>
-  
    <!------form---->
  <div class="container">
     <form class="form-horizontal k">
@@ -118,19 +115,16 @@ Session::logged();
 </div>
     <!--------- button edit------>
  <div class="buttonsdesign">
-   <button type="submit" class="sub " data-toggle="collapse" data-target="#demo" required>Submit</button>
-      
+   <button type="submit" class="sub " data-toggle="collapse" data-target="#demo" required>Submit</button>      
   <button type="cancel" class="can">Cancel</button>
 <div id="demo" class="collapse">
       <div class="alert alert-success">
   <strong>Success!</strong> Your problem is sent thanks for sharing us
 </div>      </div>
-
-     </div>
- 
+ </div>
   </div>
     </form>
  </div>
-
+<? php include ("Footer.php") ?>
   </body>
 </html>
