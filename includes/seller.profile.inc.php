1f82059e72cd43fb45bcdde3491ef01791f3df8b
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-01-10 15:43:42 +0200

    Update seller.profile.inc.php

--- includes/seller.profile.inc.php
+++ includes/seller.profile.inc.php
@@ -11,31 +11,31 @@ else if(isset($_POST['save'])){
     Seller:: updateRank();
     if($_POST['password']==null && $_POST['new']==null) {
 
-        if(Seller::updateInfo($_POST['first_name'], $_POST['last_name'], $_POST['email'])){
+        if(Seller::update($_POST['first_name'], $_POST['last_name'], $_POST['email'])){
             Session::set('success', "Changes successfully made!");
-            header("Location: ../profile.php");
+            header("Location: ../seller profile.php");
             return;
         }
         else if (!Seller::updateInfo($_POST['first_name'],$_POST['last_name'],$_POST['email'])) {
             Session::set('error',"This e-mail is already used!");
-            header("Location: ../profile.php");
+            header("Location: ../seller profile.php");
             return;
         }
     }
     else if($_POST['password']!=null && $_POST['new']!=null){
         if(Seller::updateAll($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['password'], $_POST['new'])){
             Session::set('success', "Password successfully changed!");
-            header("Location: ../profile.php");
+            header("Location: ../seller profile.php");
             return;
         }
         else if(!Seller::updateAll($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['password'], $_POST['new'])) {
             Session::set('error', "Wrong password");
-            header("Location: ../profile.php");
+            header("Location: ../seller profile.php");
         }
     }
     else{
         Session::set('error', "Please fill in all the fields");
-        header("Location: ../profile.php");
+        header("Location: ../seller profile.php");
         }
  }
 
