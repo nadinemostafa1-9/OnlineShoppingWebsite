ef2aaa0787cd5c70fa23776a902e5300e47c0806
Salma-Hamed <71777717+Salma-Hamed@users.noreply.github.com>
2021-01-11 20:50:58 +0200

    Update toys.php

--- toys.php
+++ toys.php
@@ -1,6 +1,6 @@
 <?php
 
- include ("includes/productController.php");
+include ("includes/prodFun.php");
  Session::init();
 
  ?>
@@ -25,10 +25,9 @@
   <!--BOOTSTRAP -->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
   <?php include ("Header.php");
    ?>
-   
+
   <div class="card-group">
 
 <?php displayProductsByCategory('toys') ?>
