49002fbb2ba3a6b3334c29d47e4e14089407020c
Salma-Hamed <71777717+Salma-Hamed@users.noreply.github.com>
2021-01-11 20:32:22 +0200

    Update details.php

--- details.php
+++ details.php
@@ -1,6 +1,6 @@
 <?php
 
- include ("includes/productController.php");
+include ("includes/prodFun.php");
  Session::init();
 
  ?>
@@ -29,9 +29,7 @@
   <!--BOOTSTRAP -->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-  <!-- <script type="text/javascript" src="js/rating.js">  </script> -->
 
-  <!--------------------------------------------------------------------home page image slider using bootstrap------------------------------------------------>
   <?php if(Session::get('seller_id')){
     include ("Seller_Header.php");}
     else {
@@ -43,7 +41,7 @@
 
   <?php
   $pid = $_GET['id'];
-  $product = getProductBy('id',$pid);
+  $product = Controller::getProductBy('id',$pid);
 
 
   ?>
