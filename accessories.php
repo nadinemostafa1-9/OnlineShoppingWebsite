27c6a33775c9039c7a737800adf7437a026e09e6
Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
2021-01-11 20:44:05 +0200

    Update accessories.php

--- accessories.php
+++ accessories.php
@@ -1,6 +1,6 @@
 <?php
 
- include ("includes/productController.php");
+include ("includes/prodFun.php");
  Session::init();
 
  ?>
@@ -27,7 +27,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 
- 
+  
   <?php include ("Header.php");
    ?>
 
@@ -36,11 +36,12 @@
 
   <div class="card-group">
 
-   <?php displayProductsByCategory('accessories') ?>
+<?php displayProductsByCategory('accessories') ?>
 
 </div>
 
- <?php include ("Footer.php")?>
+  <?php include ("Footer.php")?>
 
 </body>
 </html>
+
