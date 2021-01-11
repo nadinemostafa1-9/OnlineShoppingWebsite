b0e805430b87995d6b270b2b335e3132afd331eb
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-01-11 19:31:56 +0200

    Update checkout.inc.php

--- includes/checkout.inc.php
+++ includes/checkout.inc.php
@@ -2,19 +2,19 @@
 include("prodFun.php");
 require_once("class-autoload.inc.php");
 
-Session::init();
+ Session::init();
 
 if(isset($_POST['checkout'])){
 $order=new Order;
 $order::InsertData($_POST['address'], $_POST['no'], $_POST['city'], $_POST['card'],$_POST['cu']);
 header("Location: ../Place_Order.php");
-Customer::getRank();
+return;
 }
  if(isset($_POST['place'])){
    Customer::setOrders();
-      $cart=gettingCart(Session::get('customer_id'));
-   updateCount(Session::get('customer_id'));
-   removeCart(Session::get('customer_id'),$cart);
+   $cart=Controller::gettingCart(Session::get('customer_id'));
+   Controller::updateCount(Session::get('customer_id'));
+   Controller::removeCart(Session::get('customer_id'),$cart);
     echo '<script>alert("Your order has been placed successfully")
     window.location.replace("../HOME.php");
     </script>';
