ad12024035f2921d7009816bbebe4edbda15ea91
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-01-11 19:31:23 +0200

    Update and rename cartController.php to cartFun.php

--- includes/cartFun.php
+++ includes/cartFun.php
@@ -1,13 +1,13 @@
 <?php
 
-include_once ('prodFun.php');
+include_once ("class-autoload.inc.php");
 
 Session::init();
 //pressing on removing button
 if(isset($_POST["remove"]) && isset($_POST['item_number'])){
-		 $cart=gettingCart(Session::get('customer_id'));
+		 $cart=Controller::gettingCart(Session::get('customer_id'));
 
-		removeItem($_POST['item_number'],$cart,Session::get('customer_id'));
+		Controller::removeItem($_POST['item_number'],$cart,Session::get('customer_id'));
 			echo '<script>alert("The item has been removed")
 			window.location.replace("../Cart.php");
 			</script>';
@@ -16,14 +16,14 @@ if(isset($_POST["remove"]) && isset($_POST['item_number'])){
 //pressing on add to cart button
 if(isset($_POST["add_to_cart"]))
 {
-	$cart = $cart=gettingCart(Session::get('customer_id'));
+	$cart = $cart=Controller::gettingCart(Session::get('customer_id'));
 	$f = 1;
 	if($_POST['qty'] == NULL){
 		echo '<script>alert("You have to specify the number of items")
 		window.location.replace("../HOME.php");
 		</script>';
 	}else{
-	updatingCart($_GET['id'],Session::get('customer_id'),$cart,$_POST['qty'],$f);}
+	Controller::updatingCart($_GET['id'],Session::get('customer_id'),$cart,$_POST['qty'],$f);}
 	if($f){
 	echo '<script>alert("The item has been successfully added")
 				window.location.replace("../HOME.php");
@@ -54,11 +54,11 @@ foreach ($items as $value) {
  		</a>
  		<div class="card-body">
  		<h3 class="card-title">' .$product->getName().'</h3>
- 		<p class="item-price">$'.$product->getPrice().'</p>
+ 		<p class="item-price">'.$product->getPrice().' LE</p>
 		<p class="item-price">quantity: '.$qty.'</p>
  		</div>
  		<div class="add-btn"  id = "card_form">
-		<form method = "post" action ="includes/cartController.php">
+		<form method = "post" action ="includes/cartFun.php">
 		<input type ="hidden" name = "item_number" value ='.$product->getID().'>
  		<button type = "submit" name = "remove" class="card-btn">Remove</button></form>
 
