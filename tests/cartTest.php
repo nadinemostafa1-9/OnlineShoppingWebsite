caa14317dcefcd889ff3fd325cfda63908e48b7c
salma77 <salmaabdelfatah77@gmail.com>
2021-01-13 20:15:15 +0200

    Integration testing for cart class

--- tests/cartTest.php
+++ tests/cartTest.php
@@ -28,6 +28,16 @@ class CartTest extends TestCase
 
         $this->assertInternalType('array', $cart4->getitems());
     }
-    
+    public function testAddProduct(){
+        require '/home/sa/Documents/SWproject/classes/product.php';
+        require '/home/sa/Documents/SWproject/classes/cartitem.php';
+
+        $prod = new Product(0, 'Jacket', 'Men-Clothes', 500, 0, '../jacket.jpg',
+         'jacket, men, stylish, winter', 'Male Jacket, Wool, Black, Made in Turkey');
+        $cart5 = new Cart(['item1', 'item2', 'item3', 'item4']);
+        $cartitem = new CartItem($prod, 3);
+        
+        $this->assertSame($cartitem, $cart5->addProduct($prod, 3));
+    }
 }
 ?>
