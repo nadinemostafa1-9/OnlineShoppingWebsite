bad02eca784df47bcfa66d38eb278a0386b75076
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-01-13 19:16:03 +0200

    Update office.php

--- office.php
+++ office.php
@@ -40,12 +40,7 @@ include ("includes/prodFun.php");
 
 </div>
 
-  <div class="footer">
-    <br>
-    <p>Shoppera is an online store where you can get anything you imagine. While using Shoppera, you agree to have read and accepted our terms of use, cookie and privacy policy.</p>
-    <br>
-    <p>All rights reserved.</p>
-  </div>
+  
  <?php include ("Footer.php") ?>
 </body>
 </html>
